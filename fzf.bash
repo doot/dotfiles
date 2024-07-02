f15a8f4b2063e38a9041b09443c7e17b62631548
Jeremy Hauschildt <jeremy@jhauschildt.com>
2024-07-02 11:49:04 -0700

    style: update way to setting up fzf completions and key bindings

--- fzf.bash
+++ fzf.bash
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == *${HOME}/.dotfiles/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}${HOME}/.dotfiles/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "${HOME}/.dotfiles/fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "${HOME}/.dotfiles/fzf/shell/key-bindings.bash"
+eval "$(fzf --bash)"
