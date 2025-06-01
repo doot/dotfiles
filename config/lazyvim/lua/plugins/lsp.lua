8eab79748c14ed1cda5d1ad909e8c84c50d5220f
Jeremy Hauschildt <jeremy@jhauschildt.com>
2025-06-01 16:19:37 -0700

    feat: only attempt to install nil_ls (nix language server) on hosts that have 'nix' in the hostname

--- config/lazyvim/lua/plugins/lsp.lua
+++ config/lazyvim/lua/plugins/lsp.lua
@@ -1,4 +1,4 @@
-return {
+local spec = {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
@@ -30,3 +30,10 @@ return {
     },
   },
 }
+
+-- Disable nil_ls (nix language server) on hosts that do not contain "nix" in the hostname
+if string.match(vim.fn.hostname(), "nix") == nil then
+  spec.opts.servers.nil_ls = { mason = false }
+end
+
+return spec
