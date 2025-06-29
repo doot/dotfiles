2ec98b686da1c146a4a9d54bf685441773e1eb9f
Jeremy Hauschildt <jeremy@jhauschildt.com>
2025-06-28 17:30:29 -0700

    feat: disable copilot telemetry

---
+++ config/lazyvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    server_opts_overrides = {
+      settings = {
+        telemetry = {
+          telemetryLevel = "off",
+        },
+      }
+    }
+  },
+}
