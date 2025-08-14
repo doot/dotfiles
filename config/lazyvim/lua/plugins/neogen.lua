c6eb892a47b7b0a6e6900cbea278f6741be071f9
Jeremy Hauschildt <jhauschildt@linkedin.com>
2025-08-14 15:00:20 -0700

    add neogen + config, luasnip, bump lazyvim

---
+++ config/lazyvim/lua/plugins/neogen.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "danymat/neogen",
+    opts = {
+      languages = {
+        python = {
+          template = {
+            annotation_convention = "reST"
+          }
+        },
+      },
+    },
+  },
+}
