4e3a13b566e9c1f725c5a24e773b9f5183300f83
Jeremy Hauschildt <jeremy@jhauschildt.com>
2026-09-07 22:57:48 -0700

    feat: add <space>uN keybinding to toggle disabling copilot inline completions

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+Snacks.toggle({
+  name = "NES and inline_completion",
+  get = function()
+    return vim.g.sidekick_nes ~= false
+  end,
+  set = function(state)
+    vim.g.sidekick_nes = state
+    vim.lsp.inline_completion.enable(state)
+  end,
+}):map("<leader>uN")
